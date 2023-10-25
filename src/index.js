10b68e3c5a3102ed3f5a7887423f80eafb911b19
Tom Striker <toms.baugis@gmail.com>
2023-10-25 22:11:48 +0100

    main - clean up the API some and add batch painter that tries to send as few instructions as possible based on what has changed; wip

--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import colors from "./colors.js";
 // a mere proxy - to the 128 colors spelled out in the basic mode
 export const Colors = colors;
 
+let colorHex = (color, idx) => parseInt(color.slice(idx, idx + 2), 16);
+
+class Knob {
+    write = true;
+
+    constructor(note, key, x, y) {
+        this.note = note;
+        this.key = key;
+        this.x = x;
+        this.y = y;
+        this._val = null;
+        this._changed = false;
+    }
+}
+
+class Fader extends Knob {
+    type = "fader";
+    write = false;
+}
+
+class Button extends Knob {
+    type = "toggle";
+}
+
+class Pad extends Knob {
+    type = "rgb";
+}
+
 class APCMiniMk2 {
     constructor() {
         this.connected = false;
@@ -13,75 +41,56 @@ class APCMiniMk2 {
         this._sysexEnabled = false;
 
         this._listeners = [];
+        this._paintLoop = false;
 
         // wrap toggles so that when the value is set, we send the signal to the MIDI light
         this._states = {};
 
-        // makes all buttons accessible by note as well as by key name
-        this._defineButtons();
-    }
+        // buttons by note
+        this.buttons = {};
+        for (let i = 0; i < 64; i++) {
+            let x = i % 8;
+            let y = 7 - (i - x) / 8;
+            this.buttons[i] = new Pad(i, `pad${x}${y}`, x, y);
+        }
 
-    _defineButtons() {
-        // pad keys and all buttons
-        Object.values(MidiButtons).forEach(button => {
-            if (button.key == "shift") {
-                // solo doesn't have a light
-                return;
-            }
-
-            let property = {
-                get() {
-                    return this._states[button.key];
-                },
-                set: val => {
-                    if (JSON.stringify(val) == JSON.stringify(this._states[button.key])) {
-                        return;
-                    }
-
-                    if (button.color == "rgb" && typeof val == "string") {
-                        return this.fill([[button.note, button.note, val]]);
-                    }
-
-                    let [color, brightness] = [val, val ? 6 : 0];
-                    if (Array.isArray(val)) {
-                        [color, brightness] = val;
-                    }
-
-                    this._states[button.key] = val;
-
-                    if (!this.connected) {
-                        // we're not connected but we're not gonna shout about it as we already yelled on connect
-                        return;
-                    }
-
-                    if (button.color == "rgb") {
-                        this.control.noteOn(button.note, color, brightness);
-                    } else if (button.color == "single") {
-                        this.control.noteOn(button.note, val ? 127 : 0);
-                    }
-                },
-            };
-
-            Object.defineProperty(this, button.note, property);
-            Object.defineProperty(this, button.key, property);
+        // horiz simple buttons
+        ["volume", "pan", "send", "device", "up", "down", "left", "right"].forEach((key, idx) => {
+            let note = 100 + idx;
+            this.buttons[note] = new Button(note, `${key}Button`, idx, 9);
         });
+        // vert simple buttons
+        ["clipStop", "solo", "mute", "recArm", "select", "drum", "note", "stopAllClips"].forEach((key, idx) => {
+            let note = 112 + idx;
+            this.buttons[note] = new Button(note, `${key}Button`, 9, idx);
+        });
+        this.buttons[122] = new Button(122, "shiftButton", 9, 9);
 
-        // faders (read-only ofc)
-        Object.values(MidiCC).forEach(fader => {
-            Object.defineProperty(this, fader.key, {
-                get() {
-                    return this._states[fader.key];
-                },
-            });
+        // faders by note
+        this.faders = Object.fromEntries(
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(idx => {
+                return [48 + idx, new Fader(48 + idx, `fader${idx}`)];
+            })
+        );
+
+        this.allControls = [...Object.values(this.buttons), ...Object.values(this.faders)];
+        this.allControls.forEach(control => {
+            // add properties by key name so that we can reference buttons by simply going `mk2.pad33` etc
+            this[control.key] = control;
         });
     }
 
     async connect(
         options = {
             sysex: false, // set to true if you want to paint with RGB colors
+            reset: true,
+            // if paintLoop is set to true, we will start a lazy ~60fps loop that continuously sends colors to midi
+            // this is preferable when you are painting with rgb colors as sending colors in batch is way more effective
+            paintLoop: false,
         }
     ) {
         this._sysexEnabled = options.sysex;
+        this._paintLoop = options.paintLoop;
 
         this.control = new MIDIControl({
             sysex: this._sysexEnabled,
@@ -91,28 +100,28 @@ class APCMiniMk2 {
             deviceCheck: port => {
                 return port.name.indexOf("APC mini mk2 Contr") != -1;
             },
-            onMessage: message => {
-                if (message.type == "sysex") {
-                    if (message.messageTypeId == 0x61) {
-                        message.data.forEach((fader, idx) => {
-                            this._states[`fader${idx}`] = fader;
+            onMessage: evt => {
+                if (evt.type == "sysex") {
+                    if (evt.messageTypeId == 0x61) {
+                        evt.data.forEach((faderVal, idx) => {
+                            this[`fader${idx}`]._val = faderVal;
                         });
                     } else {
-                        this._dispatchEvent("sysex", message);
+                        this._dispatchEvent("sysex", evt);
                     }
                     return;
                 }
 
-                let button = message.type == "cc" ? MidiCC[message.note] : MidiButtons[message.note];
+                let button = evt.type == "cc" ? this.faders[evt.note] : this.buttons[evt.note];
 
-                if (message.type == "cc") {
+                if (evt.type == "cc") {
                     // normalize the value and round to the 6th digit as that's far enough
                     let prev = this._states[button.key];
-                    this._states[button.key] = message.value;
-                    this._dispatchEvent("cc", {...message, ...button, prevVal: prev});
+                    this[button.key]._val = evt.value;
+                    this._dispatchEvent("cc", {...evt, ...button, prevVal: prev});
                 } else {
                     // button press
-                    this._dispatchEvent(message.type, {...message, ...button});
+                    this._dispatchEvent(evt.type, {...evt, ...button});
                 }
             },
             onStateChange: event => {
@@ -121,26 +130,137 @@ class APCMiniMk2 {
         });
         await this.control.connect();
 
+        // link all the buttons
+        let propertyNames = {rgb: "color", toggle: "on", fader: "value"};
+        this.allControls.forEach(control => {
+            if (propertyNames[control.type]) {
+                Object.defineProperty(control, propertyNames[control.type], {
+                    get: () => this._getValue(control),
+                    set: value => this._setValue(control, value),
+                });
+            }
+        });
+
         if (options.sysex) {
             // if we have sysex enabled, sniff out the current slider states
-            // this.device.out.send([0xf0, 0x47, 0x7f, 0x4f, 0x60, 0x00, 0x04, 0x41, 0x09, 0x01, 0x04, 0xf7]);
+            // the 0x61 response will come to to onMessage, so check the code above
             this.control.sendSysex(0x60, [0x41, 0x09, 0x01, 0x04]);
         }
 
-        this.reset();
+        if (options.reset) {
+            this.reset();
+        }
+
+        if (options.paintLoop) {
+            this.startPaintLoop();
+        }
+    }
+
+    _getValue(control) {
+        return control._val;
+    }
+
+    _setValue(control, val) {
+        if (!control.write || JSON.stringify(val) == JSON.stringify(control._val)) {
+            return;
+        }
+        control._val = val;
+
+        if (control.type == "toggle") {
+            // toggles are simple enough
+            this.control.noteOn(control.note, val ? 127 : 0);
+        } else if (control.type == "rgb" && typeof val == "string" && val.length == 7) {
+            if (this._paintLoop) {
+                control._changed = true;
+            } else {
+                let [r, g, b] = [colorHex(val, 1), colorHex(val, 3), colorHex(val, 5)];
+                this.control.sendSysex(0x24, control.note, control.note, ...toMLSB(r), ...toMLSB(g), ...toMLSB(b));
+            }
+        } else {
+            let [color, brightness] = [val, val ? 6 : 0];
+            if (Array.isArray(val)) {
+                [color, brightness] = val;
+            }
+            this.control.noteOn(this.note, color, brightness);
+        }
+    }
+
+    startPaintLoop() {
+        let x = 0;
+        let inner = () => {
+            //console.log("eeee");
+            //let toPaint = Object.values(this.buttons).filter(button => button._changed);
+
+            let fills = [];
+
+            let curColor = null;
+            let from = null;
+            let to = null;
+
+            for (let i = 0; i < 64; i++) {
+                let button = this.buttons[i];
+                let color = button._val;
+
+                if (color === curColor) {
+                    // if we are same as the previous, we are happy to keep going
+                    to = i;
+                } else if (curColor) {
+                    // reset
+                    fills.push([from, to, curColor]);
+                    if (button._changed) {
+                        curColor = color;
+                        from = i;
+                        to = i;
+                    } else {
+                        curColor = null;
+                    }
+                } else if (button._changed) {
+                    curColor = button._val;
+                    from = i;
+                    to = i;
+                }
+
+                if (button._changed) {
+                    button._changed = false;
+                }
+            }
+            if (curColor) {
+                fills.push([from, to, curColor]);
+            }
+
+            if (fills.length) {
+                //console.log(fills.length, "fill instructions", fills);
+                this.fill(fills);
+            }
+
+            if (this._paintLoop) {
+                //requestAnimationFrame(inner);
+                requestAnimationFrame(inner);
+            }
+        };
+        inner();
+    }
+
+    select(x1, y1, x2, y2) {
+        // return a list of buttons in the selected range; goes left-to-right, top-to-bottom
+        let buttons = [];
+        for (let y = y1; y <= y2; y++) {
+            for (let x = x1; x <= x2; x++) {
+                buttons.push(this[`pad${x}${y}`]);
+            }
+        }
+        return buttons;
     }
 
     reset() {
         // turn all pads off
-        Object.values(MidiButtons).forEach(button => {
-            this[button.key] = 0;
+        Object.values(this.buttons).forEach(button => {
+            this[button.key].color = 0;
         });
     }
 
-    async fill(padColors) {
+    fill(padColors) {
         // fill
-
-        let colorHex = (color, idx) => parseInt(color.slice(idx, idx + 2), 16);
 
         if (!this._sysexEnabled) {
             throw Error(
@@ -168,7 +288,7 @@ class APCMiniMk2 {
             // discussion here: https://github.com/WebAudio/web-midi-api/issues/158
             // the best you can do is not blast, but if you do blast, use setInterval/setTimeout and manage the
             // buffer yourself
-            this.control.sendSysex(0x24, message);
+            this.control.sendSysex(0x24, ...message);
         }
     }
 
@@ -198,68 +318,12 @@ class APCMiniMk2 {
     }
 
     async disconnect() {
-        await this.reset();
+        this._paintLoop = false;
+        this.reset();
         this.control.disconnect();
-        Object.keys(MidiButtons).forEach(button => {
-            // clean up after ourselves and reset the buttons on unload
-            this[button] = false;
-        });
-        this.connected = false;
         this._listeners = [];
+        this.connected = false;
     }
 }
-
-let MidiButtons = {};
-
-// horiz simple buttons
-["volume", "pan", "send", "device", "up", "down", "left", "right"].forEach((key, idx) => {
-    let note = 100 + idx;
-    MidiButtons[note] = {
-        note,
-        color: "single",
-        key,
-        col: idx,
-        row: 9,
-    };
-});
-
-// vert simple buttons
-["clipStop", "solo", "mute", "recArm", "select", "drum", "note", "stopAllClips"].forEach((key, idx) => {
-    let note = 112 + idx;
-    MidiButtons[note] = {
-        note,
-        color: "single",
-        key,
-        col: 9,
-        row: idx,
-    };
-});
-
-MidiButtons[122] = {
-    note: 122,
-    color: false,
-    key: "shift",
-    col: 9,
-    row: 9,
-};
-
-for (let i = 0; i < 64; i++) {
-    let x = i % 8;
-    let y = 7 - (i - x) / 8;
-    MidiButtons[i] = {
-        note: i,
-        color: "rgb",
-        key: `pad${x}${y}`,
-        col: x,
-        row: y,
-    };
-}
-
-// sliders
-let MidiCC = Object.fromEntries(
-    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((slider, idx) => {
-        return [48 + idx, {key: `fader${slider}`, type: "cc"}];
-    })
-);
 
 export default APCMiniMk2;
